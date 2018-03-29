96e46a6322cc64fd01fcf0f1fd9a59a5d47ac92c
cdmccauley <cdmccauley@gmail.com>
2018-03-28 21:12:35 -0600

    csharp-highest-profit-wins passed all tests

---
+++ csharp-highest-profit-wins/csharp-highest-profit-wins/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace csharp_highest_profit_wins
+{
+    class Program
+    {
+        public static int[] minMax(int[] lst)
+        {
+            return new int[] { lst.Min(), lst.Max() };
+        }
+
+        static void Main(string[] args)
+        {
+            int[] methodOutput;
+
+            methodOutput = minMax(new int[] { 1, 2, 5, -1, 12, 20 });  // -1, 20
+            methodOutput.ToList().ForEach(Console.WriteLine);
+
+            methodOutput = minMax(new int[] { 1, 2, 3, 4, 5 });  // 1, 5
+            methodOutput.ToList().ForEach(Console.WriteLine);
+
+            methodOutput = minMax(new int[] { 1, 2, -3, 4, 5 });  // -3, 5
+            methodOutput.ToList().ForEach(Console.WriteLine);
+
+            Console.ReadKey();
+        }
+    }
+}
