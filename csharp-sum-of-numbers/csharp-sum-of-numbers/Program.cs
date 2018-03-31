0f3f10edb96614a95088f613d824908c60949b06
cdmccauley <cdmccauley@gmail.com>
2018-03-30 19:26:25 -0600

    csharp-sum-of-numbers passed all tests

---
+++ csharp-sum-of-numbers/csharp-sum-of-numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_sum_of_numbers
+{
+    class Program
+    {
+        public static int GetSum(int a, int b)
+        {
+            int sum = 0;
+
+            if (a == b)
+            {
+                return a;
+            }
+            else if (a < b)
+            {
+                for (int i = a; i <= b; ++i)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+            else
+            {
+                for (int i = b; i <= a; ++i)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSum(0, 1));
+            Console.WriteLine(GetSum(0, -1));
+
+            Console.ReadKey();
+        }
+    }
+}
