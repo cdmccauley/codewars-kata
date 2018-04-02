0991bf2a866a1c7c1f3cabaa49731e8b68e1b4b0
cdmccauley <cdmccauley@gmail.com>
2018-04-01 21:26:57 -0600

    csharp-find-the-stray-number passed all tests

---
+++ csharp-find-the-stray-number/csharp-find-the-stray-number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_find_the_stray_number
+{
+    class Program
+    {
+        public static int Stray(int[] numbers)
+        {
+            List<int> numList = numbers.ToList<int>();
+
+            numList.Sort();
+            int first = numList[0];
+            numList.RemoveAt(0);
+            if (!numList.Contains(first))
+                return first;
+            else
+            {
+                foreach (int number in numList)
+                    if (number != first)
+                        return number;
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Stray(new int[] { 1, 1, 2 }));
+
+            Console.ReadKey();
+        }
+    }
+}
