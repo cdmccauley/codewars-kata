c9e551ceb633c5a3157901e3a5233e068f845672
cdmccauley <cdmccauley@gmail.com>
2018-04-06 20:15:12 -0600

    csharp-the-wave passed all tests

---
+++ csharp-the-wave/csharp-the-wave/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_the_wave
+{
+    class Program
+    {
+        public static List<string> wave(string str)
+        {
+            List<string> theWave = new List<string>();
+            char[] currentWave;
+
+            if (str != string.Empty)
+            {
+                currentWave = str.ToCharArray();
+
+                for(int i = 0; i < currentWave.Length; ++i)
+                {
+                    if (Char.IsLetter(currentWave[i]))
+                    {
+                        currentWave[i] = Char.ToUpper(currentWave[i]);
+                        theWave.Add(new string(currentWave));
+                        currentWave[i] = Char.ToLower(currentWave[i]);
+                    }
+                }
+
+                return theWave;
+            }
+            else
+            {
+                return new List<string> { };
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (string waver in wave("hello"))
+                Console.WriteLine(waver);
+
+            Console.ReadKey();
+        }
+    }
+}
