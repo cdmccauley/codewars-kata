ed29cf576f1c5d78a29023726cbe488111239be6
cdmccauley <cdmccauley@gmail.com>
2018-04-08 18:36:21 -0600

    researching permutations for java-best-travel

--- java-best-travel/src/Main.java
+++ java-best-travel/src/Main.java
@@ -1,22 +1,19 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
-        int currentRoute;
+        int[] currentRoute = new int[k];
+        ArrayList<Integer> possibleRoutes = new ArrayList<>();
 
         if ((ls.size() == 1 && ls.get(0) == null) || ls.size() < k) {
             System.out.println("line 11"); /// DEBUG !!!
             return null;
         } else {
-            for (int i = 0; i < ls.size() - 1; i++) {
-                currentRoute = ls.get(i);
-                for (int j = i; j < ls.size() - 1; j++) {
-                    // working on algorithm...
-                }
-            }
+            // https://en.wikipedia.org/wiki/Heap%27s_algorithm researching permutations
         }
 
         return -1;
