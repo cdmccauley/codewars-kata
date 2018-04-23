ac187569bb6362dcf6b54284fe5d93c91e2e57c8
cdmccauley <cdmccauley@gmail.com>
2018-04-22 20:59:51 -0600

    csharp-longest-word passed all tests

---
+++ csharp-longest-word/csharp-longest-word/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace csharp_longest_word
+{
+    class Program
+    {
+        public static string LongestWord(string sen)
+        {
+            string[] words = Regex.Matches(sen, @"\b\w+\b").Cast<Match>().Select(m => m.Value).ToArray();
+
+            return words.OrderByDescending(s => s.Length).First();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestWord("fun&!! time"));
+            Console.WriteLine(LongestWord("I love dogs"));
+
+            Console.ReadKey();
+        }
+    }
+}
